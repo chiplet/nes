a8aa6b79e9ea0c1e9ec9667767a031b5781f273a
Verneri Hirvonen <verneri.hirvonen.2@gmail.com>
2020-12-22 00:07:23 +0200

    WIP start adding assembly printout formatting.

--- src/cpu/isa.rs
+++ src/cpu/isa.rs
@@ -1544,6 +1544,34 @@ impl Instruction {
         }
     }
 }
+impl fmt::Display for Instruction {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        const WIDTH_1: usize = 12;
+        const WIDTH_2: usize = 10;
+
+        for byte in &self.machine_code {
+            write!(f, "{:02x} ", byte).unwrap();
+        }
+        let spacing = " ".repeat(WIDTH_1-self.machine_code.len()*3);
+        write!(f, "{}{} ", spacing, self.name.mnemonic);
+
+        match &self.addr_mode {
+            AddrMode::A => write!(f, "{}", " ".repeat(WIDTH_2-3)),
+            AddrMode::Abs(addr) => write!(f, "${:04x}{}", addr, " ".repeat(WIDTH_2-5)),
+            AddrMode::AbsX(addr) => write!(f, "${:04x},X{}", addr, " ".repeat(WIDTH_2-7)),
+            AddrMode::AbsY(addr) => write!(f, "${:04x},Y{}", addr, " ".repeat(WIDTH_2-7)),
+            AddrMode::Imm(value) => write!(f, "#${:02x}{}", value, " ".repeat(WIDTH_2-3)),
+            AddrMode::Impl => write!(f, "{}", " ".repeat(WIDTH_2-3)),
+            AddrMode::Ind(addr) => write!(f, "${:04x}", addr),
+            AddrMode::XInd(addr) => write!(f, "${:04x}", addr),
+            AddrMode::IndY(addr) => write!(f, "${:04x}", addr),
+            AddrMode::Rel(value) => write!(f, "${:02x}", value),
+            AddrMode::Zpg(addr) => write!(f, "${:04x}", addr),
+            AddrMode::ZpgX(addr) => write!(f, "${:04x},X", addr),
+            AddrMode::ZpgY(addr) => write!(f, "${:04x},Y", addr),
+        }
+    }
+}
 
 
 /** decoding helpers **/
