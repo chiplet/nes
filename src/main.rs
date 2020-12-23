f8f0ed498a118399b79f3cc26a44163994a928d6
Verneri Hirvonen <verneri.hirvonen.2@gmail.com>
2020-12-23 20:45:15 +0200

    Add memory loading from easy6502 hexdumps

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 mod cpu;
+mod util;
 use crate::cpu::CPU;
 
 fn main() {
     let mut cpu = CPU::init();
 
+    cpu.load_hexdump("./hexdumps/test.txt").unwrap();
+    cpu.pc = 0x0600;
+
     loop {
         cpu.tick().unwrap();
     }
 
-    println!("ram[${:04x}] = ${:02x}", 0x200, cpu.ram[0x0200]);
+    // println!("ram[${:04x}] = ${:02x}", 0x200, cpu.ram[0x0200]);
 }
