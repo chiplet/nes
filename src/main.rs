bb4df6a177732ae18dbafcff09befa34b97a5ed0
Verneri Hirvonen <verneri.hirvonen.2@gmail.com>
2020-12-31 16:30:25 +0200

    Panic if CPU tick returns an error

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ fn main() {
     cpu.load_ines("./hexdumps/tests/nestest.nes").unwrap();
     cpu.pc = 0xc000;
     loop {
-        cpu.tick();
+        cpu.tick().unwrap();
     }
 
     // println!("ram[${:04x}] = ${:02x}", 0x200, cpu.ram[0x0200]);
